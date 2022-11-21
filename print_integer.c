551db5b3cf8f0ccf78b8cfa3956447528dd95765
Esslam24 <113611975+Esslam24@users.noreply.github.com>
2022-11-21 19:10:52 +0200

    Update and rename get_width.c to print_integer.c

---
+++ print_integer.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int print_integer(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, int), 10);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
+Footer
+© 2022 GitHub, Inc.
